2f897493e9d4853609131115e5b371441a14f200
Ngo Van Thuc <ngothuc0704@gmail.com>
2024-05-04 22:58:19 +0700

    ex2

--- ex2/src/App.js
+++ ex2/src/App.js
@@ -3,18 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className='grid-container'>
-      <div className='grid-item-1'>1</div>
-      <div className='grid-item'>2</div>
-      <div className='grid-item'>3</div>
-      <div className='grid-item'>4</div>
-      <div className='grid-item'>5</div>
-      <div className='grid-item'>6</div>
-      <div className='grid-item'>7</div>
-      <div className='grid-item'>8</div>
-      <div className='grid-item'>9</div>
-      <div className='grid-item'>10</div>
-      <div className='grid-item'>11
+    <div className='container'>
+      <div className='grid-container'>
+        <div className='grid-item-1'></div>
+        <div className='grid-item-2'></div>
+        <div className='grid-item-3'></div>
+        <div className='grid-item-4'></div>
+        <div className='grid-item-5'></div>
+        <div className='grid-item-6'></div>
       </div>
     </div>
   );
